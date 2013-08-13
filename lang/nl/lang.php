9e3b7298e8aa8302438f0da24d493d1ace8bac02
Mark Prins <mprins@users.sf.net>
2013-08-13 13:10:17 +0200

    translation update

---
+++ lang/nl/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Mark Prins <mprins@users.sf.net>
+ */
+$lang['editionLinkTitle']      = 'deze paragraaf in de %s editie';
