80347b828eec256d205226c2db414b5858f34aed
İlker Rifat Kapaç <irifat@gmail.com>
2014-05-16 10:15:36 +0200

    translation update

---
+++ lang/tr/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author İlker Rifat Kapaç <irifat@gmail.com>
+ */
+$lang['editionLinkTitle']      = 'bu paragraf, %s sürümünün içindedir';
