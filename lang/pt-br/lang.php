af5a3b521992b09dda54689e926c095a53a03825
Alexandre Belchior <alexbelchior@gmail.com>
2018-07-05 13:40:48 +0200

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Alexandre Belchior <alexbelchior@gmail.com>
+ */
+$lang['editionLinkTitle']      = 'este parágrafo na edição %s';
